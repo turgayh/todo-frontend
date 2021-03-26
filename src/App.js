835f7806619cd363e26ab81d2b1e2b10a1d76022
hakan turğay <turgayh@itu.edu.tr>
2021-03-27 00:52:27 +0300

    test green

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import './App.css';
+import { useState } from 'react'
+import "./App.css"
+
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <div className="input-form">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          name="input-task"
+          onChange={e => setValue(e.target.value)}
+        />
+        <button type='submit' name="button-add-task">Add</button>
+      </form>
+    </div>
+  );
+}
+
 
 function App() {
+  const [listOfTask, setlistOfTask] = useState([])
+
+  const addTodo = text => {
+    const newTodos = [...listOfTask, { text }];
+    setlistOfTask(newTodos);
+  };
+
+
+
   return (
     <div className="App">
+      <TodoForm addTodo={addTodo} />
+
+      <div className="todo-list">
+        {listOfTask.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
 
     </div>
   );
