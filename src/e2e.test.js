11ad47cb070bdc36f2df38fc0cd9b19905036102
hakan turğay <turgayh@itu.edu.tr>
2021-03-27 00:49:35 +0300

    acceptance test

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -7,26 +7,33 @@ beforeAll(async () => {
     page = await browser.newPage()
 })
 describe('Todo App', () => {
-    test('Task added result added to list of task', async () => {
+    test('Added new task to empty task list successfully', async () => {
+        const newTask = "buy some milk";
+
         await page.goto(`${appUrlBase}/`)
-        await page.$('[data-test="input-task"]', el => el.value = 'buy some milk');
-        const taskAddBtn = await page.$('[data-test="button-add-task"]')
-        const initialAllTask = await page.evaluate(() =>
-            Array.from(
-                document.querySelectorAll('#task-list ul li'),
-                (element) => element.textContent
-            )
-        )
-        await taskAddBtn.click()
+        await page.waitForSelector('.input-form');
+        await page.click("input[name=input-task]");
+        await page.type("input[name=input-task]", newTask);
+        await page.click("button[name=button-add-task]");
+        const task = await page.$eval('.todo', elem => elem.innerHTML)
+        expect(task).toEqual(newTask);
+    })
 
-        const afterClickAllTask = await page.evaluate(() =>
-            Array.from(
-                document.querySelectorAll('#task-list ul li'),
-                (element) => element.textContent
-            )
-        )
+    test('Added new task to non-empty task list successfully', async () => {
+        const firstTask = "buy some milk";
+        const secondTask = "buy some milk 2";
 
-        expect(afterClickAllTask[0]).toEqual(initialAllTask[0]);
+        await page.goto(`${appUrlBase}/`)
+        await page.waitForSelector('.input-form');
+        await page.click("input[name=input-task]");
+        await page.type("input[name=input-task]", firstTask);
+        await page.click("button[name=button-add-task]");
+        await page.click("input[name=input-task]");
+        await page.type("input[name=input-task]", secondTask);
+        await page.click("button[name=button-add-task]");
+        const task = await page.$eval('.todo:nth-child(2)', elem => elem.innerHTML)
+        expect(task).toEqual(secondTask);
+
     })
 })
 afterAll(() => {
