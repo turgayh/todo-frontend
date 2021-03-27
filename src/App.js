396d9fc231f672358af66c247948eacdf0a80da8
hakan turğay <turgayh@itu.edu.tr>
2021-03-27 22:21:20 +0300

    Refactoring App.js (#4)
    * check 3
    
    * refactoring App.js
    
    * react down version & added test cases

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
-import { useState } from 'react'
 import "./App.css"
-import { ListTask } from './components/index';
-
-
-
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <div className="input-form">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="input"
-          value={value}
-          name="input-task"
-          onChange={e => setValue(e.target.value)}
-        />
-        <button type='submit' name="button-add-task">Add</button>
-      </form>
-    </div>
-  );
-}
-
-
+import TodoAppPage from "./containers/TodoAppPage"
 function App() {
-  const [listOfTask, setlistOfTask] = useState([])
-
-  const addTodo = text => {
-    const newTodos = [...listOfTask, { text }];
-    setlistOfTask(newTodos);
-  };
-
-
 
   return (
     <div className="App">
-      <TodoForm addTodo={addTodo} />
-      <ListTask tasks={listOfTask} />
+      <TodoAppPage />
     </div>
   );
 }
--- src/components/ListTask.test.js
+++ src/components/ListTask.test.js
@@ -1,9 +1,9 @@
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { ListTask } from './index';
 describe("Component ListTask", () => {
     test("ListTask should be render", () => {
         const wrapper = shallow(<ListTask tasks={[{ key: 1, index: 1, todo: "task 1" }]} />);
-        expect(wrapper.find("#root > div > div.todo-list > div:nth-child(1)")).toHaveLength(0)
+        expect(wrapper.exists()).toBe(true)
 
     })
 })
