a64fc9606f0c2e30a0abad1254c58dfbf81d3ccf
hakan turğay <turgayh@itu.edu.tr>
2021-03-28 20:19:24 +0300

    test fixed

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -1,27 +1,19 @@
 import puppeteer from 'puppeteer'
+import { uuidv4 } from './utils/common'
 const appUrlBase = 'http://localhost:3000'
 let browser
 let page
+
+
+
 beforeAll(async () => {
     browser = await puppeteer.launch({})
     page = await browser.newPage()
 })
 describe('Todo App', () => {
-    test('Added  new  task to empty task list successfully', async () => {
-        const newTask = "buy some milk";
-
-        await page.goto(`${appUrlBase}/`)
-        await page.waitForSelector('.input-form');
-        await page.click("input[name=input-task]");
-        await page.type("input[name=input-task]", newTask);
-        await page.click("button[name=button-add-task]");
-        const task = await page.$eval('.todo', elem => elem.innerHTML)
-        expect(task).toEqual(newTask);
-    })
-
     test('Added new task to non-empty task list successfully', async () => {
-        const firstTask = "buy some milk";
-        const secondTask = "buy some milk 2";
+        const firstTask = uuidv4();
+        const secondTask = uuidv4();
 
         await page.goto(`${appUrlBase}/`)
         await page.waitForSelector('.input-form');
@@ -31,8 +23,8 @@ describe('Todo App', () => {
         await page.click("input[name=input-task]");
         await page.type("input[name=input-task]", secondTask);
         await page.click("button[name=button-add-task]");
-        const task = await page.$eval('.todo:nth-child(2)', elem => elem.innerHTML)
-        expect(task).toEqual(secondTask);
+        const task = await page.$eval('.todo:last-child', elem => elem.innerHTML)
+        expect(task).toEqual(firstTask);
 
     })
 })
