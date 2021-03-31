c17095ff7ba69b4bec30a330d8f3de6755683c52
hakan turğay <turgayh@itu.edu.tr>
2021-03-31 13:31:10 +0300

    Refactoring (#8)
    * check 3
    
    * refactoring & change test
    
    * test name change
    
    * test enviroment

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
-const HOST = "https://todo-backend-bhl6p.ondigitalocean.app"
-const PORT = ""
+const HOST = process.env.REACT_APP_HOST
+const PORT = process.env.REACT_APP_PORT ?? ""
 const CREATE_TASK = HOST + PORT + "/v1/todo/create";
 const GET_ALL_TASK = HOST + PORT + "/v1/todo/get-all";
 
